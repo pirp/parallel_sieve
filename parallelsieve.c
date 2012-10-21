341cc2b29289e5847af00562176372474a31fdf4
Kleopatra <kleopatra.pirpinia@gmail.com>
2012-10-21 14:46:04 +0200

    update

--- parallelsieve.c
+++ parallelsieve.c
@@ -13,33 +13,52 @@ void parSieve(){
 	bsp_begin(P);
 	int p,s;
 	int m=N/2;
-	int size_seq_list=sqrt(N);
+	int q=sqrt(N);
 	p=bsp_nprocs();
 	s=bsp_pid();
 
-	int* list=seq(size_seq_list);
+	int* list=seq(q);
 	
 	int i,j;
 	int count=0;
 
 
-	for(i=0;i<size_seq_list;i++) if(list[i]!=0) count++; 
+	for(i=0;i<q;i++) if(list[i]!=0) count++; 
 
-	int* primes_until_sqrootn=vecalloci(count);
+	int* initial_primes = vecalloci(count);
 	
 	i=0;
-	for(j=0;j<size_seq_list;j++) {
+	for(j=0;j<q;j++) {
 		if(list[j]!=0) 
 		{
-			primes_until_sqrootn[i]=list[j];
+			initial_primes[i]=list[j];
 			i++;
 
 		}
 	}
 
-	printf("%d The number of primes below %d is %d \n",s,(int)sqrt(N),count);
-	//for(i=0;i<count;i++) printf("%d: l[%d]=%d\n",s,i,primes_until_sqrootn[i]);
-	//int nloc=(N-size_seq_list)/p;
+//	printf("%d The number of primes below %d is %d \n",s,(int)sqrt(N),count);
+	double ratio = (double)(N-q)/(2*p);
+	int b=ceil(ratio); // number of local element
+	
+	printf("local elements %d\n",b);
+
+
+	int* local=vecalloci(b);
+	
+	i=0;
+	j=q+2*s*b;
+
+	if(j%2==0) j++;
+	
+	while( i<b && j<N ){ 
+		local[i]=j;
+		i++;
+		j+=2;
+	}
+
+	if(s==3) for(i=0;i<b;i++) printf("%d: l[%d]=%d\n",s,i,local[i]);
+
 	bsp_end();
 }
 
