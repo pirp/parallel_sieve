67e4281a65beb616c2dc7a59a71fb91614a60dcf
to-pirp <kleopatra.pirpinia@gmail.com>
2012-10-23 11:58:44 +0200

    outputs added

--- bspmpi/jobscripts/bench/get/bench_get.sh
+++ bspmpi/jobscripts/bench/get/bench_get.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #cd /home/bissstud/Students11/d.taviani/parAlg/BSPedupack
 #mpcc -o bench_get bspbench_get.c bspedupack.c -lbsponmpi
-cd ../../../BSPedupack
+cd ../../../BSPedupack1.01
 mpcc -o bench_get bspbench_get.c bspedupack.c -lbsponmpi
 ./bench_get $1
--- bspmpi/jobscripts/bench/put/bench.sh
+++ bspmpi/jobscripts/bench/put/bench.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 #cd /home/bissstud/Students11/d.taviani/parAlg/BSPedupack
-cd ../../../BSPedupack
+cd ../../../BSPedupack1.01
 mpcc -o bench_put bspbench.c bspedupack.c -lbsponmpi
 ./bench_put $1 
